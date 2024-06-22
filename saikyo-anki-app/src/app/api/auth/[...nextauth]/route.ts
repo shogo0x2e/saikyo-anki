34bbf50de0f5d431e468befb9535ff08458a70e4
Shogo Kitada <contact@shogo0x2e.com>
2024-06-22 19:10:55 +0900

    add prisma

--- saikyo-anki-app/src/app/api/auth/[...nextauth]/route.ts
+++ saikyo-anki-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+
+const prisma = new PrismaClient();
+
 export const authOptions = {
   // Configure one or more authentication providers
   secret: process.env.NEXTAUTH_SECRET as string,
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -11,6 +17,6 @@ export const authOptions = {
   ],
 }
 
-const handler =  NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
